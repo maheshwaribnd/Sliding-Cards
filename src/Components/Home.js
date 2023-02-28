cc7aece731c229eb3ae43798b9e7d66bde7a8f4f
maheshwaribnd <mamtabnd097@gmail.com>
2023-02-28 23:37:46 +0530

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Cards from './Cards';
+import { cardArray } from './detailsArray';
 
 
 function HomePage() {
@@ -40,7 +41,7 @@ function HomePage() {
         ]
     };
 
-    const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
 
     // useEffect(() => {
     //     axios
@@ -56,40 +57,8 @@ function HomePage() {
 
     return (
         <div style={{ width: '74%', height: '100%' }} className=' mx-auto mt-6 px-2 sm:w-10/12'>
-            {/* {data.map((cardData) => (
-                <Cards cardData={cardData}/>
-            ))} */}
             <Slider {...settings}>
-                <Cards
-                    img='https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-                    title='Card Title 1'
-                    description='Some quick example text to build on the card title and make up the bulk of the card'
-                />
-                <Cards
-                    img='https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-                    title='Card Title 2'
-                    description='Some quick example text to build on the card title and make up the bulk of the card'
-                />
-                <Cards
-                    img='https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-                    title='Card Title 3'
-                    description='Some quick example text to build on the card title and make up the bulk of the card'
-                />
-                <Cards
-                    img='https://images.unsplash.com/photo-1617396900799-f4ec2b43c7ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                    title='Card Title 4'
-                    description='Some quick example text to build on the card title and make up the bulk of the card'
-                />
-                <Cards
-                    img='https://images.unsplash.com/photo-1640833906651-6bd1af7aeea3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
-                    title='Card Title 5'
-                    description='Some quick example text to build on the card title and make up the bulk of the card'
-                />
-                <Cards
-                    img='https://images.unsplash.com/photo-1620321023374-d1a68fbc720d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1197&q=80'
-                    title='Card Title 6'
-                    description='Some quick example text to build on the card title and make up the bulk of the card'
-                />
+                {cardArray.map((item) => (<Cards img={item.img} title={item.title} description={item.description} />))}
             </Slider>
         </div>
     )
---
+++ src/Components/detailsArray.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const cardArray = [
+    {
+        img: 'https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        title: 'Card Title 1',
+        description: 'Some quick example text to build on the card title and make up the bulk of the card'
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        title: 'Card Title 2',
+        description: 'Some quick example text to build on the card title and make up the bulk of the card'
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        title: 'Card Title 3',
+        description: 'Some quick example text to build on the card title and make up the bulk of the card'
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1617396900799-f4ec2b43c7ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        title: 'Card Title 4',
+        description: 'Some quick example text to build on the card title and make up the bulk of the card'
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1640833906651-6bd1af7aeea3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
+        title: 'Card Title 5',
+        description: 'Some quick example text to build on the card title and make up the bulk of the card'
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1620321023374-d1a68fbc720d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1197&q=80',
+        title: 'Card Title 6',
+        description: 'Some quick example text to build on the card title and make up the bulk of the card'
+    }
+];
